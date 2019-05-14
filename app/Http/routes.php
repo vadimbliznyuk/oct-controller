e2478c04581c81431f6895a0aea3545d5a362b54
Vasyl <admin@gmail.com>
2019-05-14 20:57:18 +0300

    migration reletions

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -14,3 +14,14 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::auth();
+
+Route::get('/home', 'HomeController@index');
+
+Route::get('/tasks', 'TaskController@index')->name('tasks_index');
+Route::post('/tasks', 'TaskController@store')->name('tasks_store');
+Route::delete('/tasks{task}', 'TaskController@destroy')->name('tasks_destroy');
+
+
+Route::resource('news', 'NewsController');
